1132f953f2f0d6a5d8961fa9c7b5cf46103b7e06
kiyan631 <80814660+kiyan631@users.noreply.github.com>
2024-10-01 17:55:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const { v4: uuidv4 } = require("uuid");
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+
+server.listen(3030);
